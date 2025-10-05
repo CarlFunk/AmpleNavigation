4b375b02713c3684ce6df706413291ebb33b07f8
Carl Funk <carl.funk@outlook.com>
2025-10-05 18:14:57 -0500

    Consolidate NavigationCoordinatorView into one

--- Sources/AmpleNavigation/NavigationCoordinatorView.swift
+++ Sources/AmpleNavigation/NavigationCoordinatorView.swift
@@ -13,9 +13,9 @@ import SwiftUI
 /// flow navigations after receiving a deeplink. Typically this would only require the most root
 /// coordinator to be managed by the developer.
 public struct NavigationCoordinatorView<Screen: NavigationScreen, ScreenView: View>: View {
-    private var coordinator: NavigationCoordinator<Screen>
+    @Bindable private var coordinator: NavigationCoordinator<Screen>
     
-    private let screen: Screen
+    private let navigation: Navigation<Screen>
     private let screenView: (_ navigation: Navigation<Screen>, _ coordinator: NavigationCoordinator<Screen>) -> ScreenView
     
     public init(
@@ -24,14 +24,65 @@ public struct NavigationCoordinatorView<Screen: NavigationScreen, ScreenView: Vi
         @ViewBuilder screenView: @escaping (_ navigation : Navigation<Screen>, _ coordinator: NavigationCoordinator<Screen>) -> ScreenView
     ) {
         self.coordinator = coordinator
-        self.screen = screen
+        self.navigation = Navigation(screen: screen)
+        self.screenView = screenView
+    }
+    
+    private init(
+        navigation: Navigation<Screen>,
+        coordinator: NavigationCoordinator<Screen>,
+        @ViewBuilder screenView: @escaping (_ navigation : Navigation<Screen>, _ coordinator: NavigationCoordinator<Screen>) -> ScreenView
+    ) {
+        self.navigation = navigation
+        self.coordinator = coordinator
         self.screenView = screenView
     }
     
     public var body: some View {
-        InternalNavigationCoordinatorView(
-            navigation: Navigation(screen: screen),
-            coordinator: coordinator,
-            screenView: screenView)
+        NavigationStack(path: $coordinator.pushPresentation) {
+            screenView(navigation, coordinator)
+                .environment(coordinator)
+                .navigationDestination(for: Navigation<Screen>.self) { navigation in
+                    screenView(navigation, coordinator)
+                        .environment(coordinator)
+                }
+                .sheet(
+                    item: Binding(get: {
+                        (coordinator.modalPresentation?.isSheet ?? false) ? coordinator.modalPresentation : nil
+                    }, set: { modalPresentation in
+                        guard modalPresentation == nil else { return }
+                        WindowRedraw.force()
+                        Task {
+                            try await coordinator.dismiss()
+                        }
+                    })
+                ) { sheetPresentation in
+                    NavigationCoordinatorView(
+                        navigation: sheetPresentation.navigation,
+                        coordinator: sheetPresentation.coordinator,
+                        screenView: { navigation, coordinator in
+                            screenView(navigation, coordinator)
+                        })
+                }
+                .fullScreenCover(
+                    item: Binding(get: {
+                        (coordinator.modalPresentation?.isModal ?? false) ? coordinator.modalPresentation : nil
+                    }, set: { modalPresentation in
+                        guard modalPresentation == nil else { return }
+                        WindowRedraw.force()
+                        Task {
+                            try await coordinator.dismiss()
+                        }
+                    })
+                ) { modalPresentation in
+                    NavigationCoordinatorView(
+                        navigation: modalPresentation.navigation,
+                        coordinator: modalPresentation.coordinator,
+                        screenView: { navigation, coordinator in
+                            screenView(navigation, coordinator)
+                        })
+                }
+        }
+        .navigationSplitViewStyle(.balanced)
     }
 }
